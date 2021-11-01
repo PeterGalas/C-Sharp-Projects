cc1da43401e68a63e21b96ebc14aa6c9aed8a107
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-01 18:12:06 -0400

    CALLING METHODS

---
+++ CALLING METHODS SUBMISSION ASSIGNMENT/CALLING METHODS SUBMISSION ASSIGNMENT/Maths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
+{
+    class Maths
+    {
+        public void Multi(int multiplyNum)
+        {
+            Console.WriteLine(multiplyNum * 162);
+        }
+
+        public void Adding(int additionNum)
+        {
+            Console.WriteLine(additionNum + 55);
+
+        }
+
+        public void Divide(double divideNum)
+        {
+            Console.WriteLine(divideNum / 4);
+
+        }
+    }
+}
