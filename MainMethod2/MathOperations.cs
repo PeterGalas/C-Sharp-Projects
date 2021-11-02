6a86fc3d295e52c5e5022a0d93eafd833365420c
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-02 15:26:26 -0400

    method submission
    page 280

---
+++ MainMethod2/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    public class Math
+    {
+        public static void mathTime(int opInt)
+        {
+            int addNum = 37;
+            int resAdd = addNum + opInt;//adds 37 + 95 = 132
+            Console.WriteLine(resAdd);
+        }
+
+        public static void mathTime(decimal opDec)
+        {
+            decimal decMult = opDec * 15;
+            Console.WriteLine(Convert.ToInt32(decMult));//rounded number 648.21m decimal * 15 = 9,723
+        }
+
+
+        public static void mathTime(string opStr)
+        {
+            decimal strAdd = Convert.ToInt32(opStr) + 100;//9000 + 100 = 9100
+            Console.WriteLine(strAdd);
+        }
+    }
+}
---
+++ MainMethod2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int opInt = 95;
+            Math.mathTime(opInt);
+
+            decimal opDec = 648.21m;
+            Math.mathTime(opDec);
+
+            string opString = "9000";
+            Math.mathTime(opString);
+
+            Console.ReadLine();
+        }
+    }
+}
