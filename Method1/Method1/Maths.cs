b073d8c76820b7988f03ba51f13b16ca46f7003a
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-02 14:12:22 -0400

    main method
    page 279

---
+++ Method1/Method1/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Maths
+    {
+        public void Multi(int multiNum)
+        {
+            Console.WriteLine(multiNum * 75);
+        }
+
+        public void Adding(int addNum)
+        {
+            Console.WriteLine(addNum + 78);
+        }
+        public void Subtracting(int subNum)
+        {
+            Console.WriteLine(subNum - 13);
+        }
+        public void Divide(double divNum)
+        {
+            Console.WriteLine(divNum / 5);
+        }
+    }
+}
---
+++ Method1/Method1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number you picked times 75 is: ");
+
+            Maths multi = new Maths();
+            multi.Multi(number);
+            Console.ReadLine();
+
+            Console.WriteLine("Your number plus 78 equals: ");
+
+            Maths add = new Maths();
+            add.Adding(number);
+            Console.ReadLine();
+
+            Console.WriteLine("The number you picked subtracted by 13 equals: ");
+
+            Maths subtract = new Maths();
+            subtract.Subtracting(number);
+            Console.ReadLine();
+            
+
+            Console.WriteLine("The number you entered, divided by 5 equals: ");
+
+            Maths divi = new Maths();
+            divi.Divide(number);
+            Console.ReadLine();
+
+        }
+    }
+}
