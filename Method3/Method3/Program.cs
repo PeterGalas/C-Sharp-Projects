d783ccac67ac699acfca7de74639acf0986ac9b5
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-02 16:59:15 -0400

    Method3
    page 281

---
+++ Method3/Method3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Operation FirstOp = new Operation();
+                Console.WriteLine("Please choose a number:");
+                FirstOp.num1 = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+
+                    Console.WriteLine("Pick a second number. Press Enter button if you do not want a second number:");
+                    FirstOp.num2 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    FirstOp.num2 = 38;
+
+                }
+                int sum = FirstOp.MathMethod(FirstOp.num1, FirstOp.num2);
+                Console.WriteLine(sum);
+
+                Console.WriteLine("Thanks you!");
+                Console.ReadLine();
+
+            }
+        }
+}
