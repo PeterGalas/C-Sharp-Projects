98965123ab4cb376c4451ecf70e98bbeb92386c1
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-03 14:11:39 -0400

    update

--- RevisionMethod2/RevisionMethod2/Properties/AssemblyInfo.cs
+++ RevisionMethod2/RevisionMethod2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Method2")]
+[assembly: AssemblyTitle("RevisionMethod2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Method2")]
+[assembly: AssemblyProduct("RevisionMethod2")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3c3292ac-c739-4d7d-8d25-4e17567e0698")]
+[assembly: Guid("0085b3bf-572d-4ac1-b7dd-e8aee42d6894")]
 
 // Version information for an assembly consists of the following four values:
 //
