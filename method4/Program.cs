0b9509ef81100629843b2b356d3c27b0c0bd8c8e
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-03 15:40:48 -0400

    CLASS SUBMISSION ASSIGNMENT
    page 285

---
+++ method4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method4
+{
+    class Program
+    {
+        public static object StaticClass { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Class1 mathObject = new Class1(); // Instantiating an object
+
+            Console.WriteLine("Select a number: ");
+            string user = Console.ReadLine();
+            int One = Convert.ToInt32(user);
+
+            mathObject.MyMethod(One); // Calling the method
+            Console.ReadLine();
+
+
+            Class1 mathObject2 = new Class1();
+            // local variable defintion
+            int operation1, operation2;
+
+            // Calling a method to get the values
+            mathObject2.GetValue(out operation1, out operation2);
+
+            Console.WriteLine("After our first method call, returning the value of a : {0}", operation1);
+            Console.WriteLine("After  our second method call, returns the value of b : {0}", operation2);
+            Console.ReadLine();
+
+
+
+            // Calling static method
+            StaticClass.ToString();
+
+
+
+        }
+
+    }
+}
---
+++ method4/StaticClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method4
+{ 
+    static class StaticClass
+{ 
+     
+    static string F_Name = "Peter Jr.";
+    static string L_Name = "Galas";
+
+    public static void details()
+    {
+        Console.WriteLine("First name: " + F_Name);
+        Console.WriteLine("Last name: " + L_Name);
+        Console.ReadLine();
+    }
+
+
+}
+}
