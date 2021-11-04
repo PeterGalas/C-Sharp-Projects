9c4ccf5051d15dca3245e3218a8ad4deaded8cf9
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-04 12:31:04 -0400

    Method and objects
    page 292

---
+++ Methods_And_Objects/Methods_And_Objects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_And_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1) { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
