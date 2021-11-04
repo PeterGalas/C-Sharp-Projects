e16d9df16bd484de496af1e66b2b0b1d5eebeb6e
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-04 15:42:34 -0400

    Abstract Class
    page 302

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+
+    public class Employee<T> : Names, ExitProgram
+    {
+    
+
+        public int Id { get; set; }
+
+        public List<T> things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Do you want to exit? (y/n)");
+            char yesOrNo = Convert.ToChar(Console.ReadLine());
+            if (yesOrNo == 'y')
+            {
+                Console.WriteLine("Alright See you later!");
+                System.Environment.Exit(0);
+            }
+            else if (yesOrNo == 'n')
+            {
+                Console.WriteLine("No problem...");
+            }
+        }
+        public static bool operator ==(Employee<T> firstEmployee, Employee<T> secondEmployee)
+        {
+            if (firstEmployee.Id == secondEmployee.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee<T> firstEmployee, Employee<T> secondEmployee)
+        {
+            if (firstEmployee.Id != secondEmployee.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Employee<string> sampleEmployee = new Employee<string>();
+                sampleEmployee.FirstName = "Sample";
+                sampleEmployee.LastName = "Student";
+                sampleEmployee.Id = 1;
+                sampleEmployee.things = new List<string>() { "Mac Book", "Keyboard", "Mouse", "Windows", "Internet" };
+                sampleEmployee.SayName();
+                Console.WriteLine("Employee ID: " + Convert.ToString(sampleEmployee.Id));
+                Console.WriteLine("First Employee's  wish List is :");
+                foreach (string tool in sampleEmployee.things)
+                {
+                    Console.WriteLine(tool);
+                }
+
+                Employee<string> employeeTwo = new Employee<string>();
+                employeeTwo.Id = 1;
+                bool sameIDs = employeeTwo == sampleEmployee;
+                Console.WriteLine("sampleEmployee and employeeTwo have identical IDs: " + Convert.ToString(sameIDs));
+                Employee<int> employeeThree = new Employee<int>();
+                employeeThree.things = new List<int>() { 7, 21, 14 };
+                Console.WriteLine("Employee Three's Favorite Numbers:");
+                foreach (int num in employeeThree.things)
+                {
+                    Console.WriteLine(Convert.ToString(num));
+                }
+
+                sampleEmployee.Quit();
+
+                ExitProgram quitTime = new Employee<double>();
+
+
+            Console.WriteLine("It's time to go...");
+            quitTime.Quit();
+
+            }
+        
+    }
+}
