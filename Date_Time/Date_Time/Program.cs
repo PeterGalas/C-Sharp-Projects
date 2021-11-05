7ed2e8a9c200760b8a0d44015d1c2a83e2e99dce
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-05 12:34:24 -0400

    Time and Date
    page 369

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+            Console.WriteLine("At this moment in time, it is: " + current);
+            Console.WriteLine("Enter a number: ");
+            string userNum = Console.ReadLine();
+            int userInt = Convert.ToInt32(userNum);
+            DateTime futureTime = current.AddHours(userInt);
+
+            Console.WriteLine("In " + userInt + " hours, it will be " + futureTime);
+            Console.ReadLine();
+        }
+    }
+}
