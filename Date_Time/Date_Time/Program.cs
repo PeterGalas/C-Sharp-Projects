aeed3f39e2451286f2238da4269000e9768d74aa
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-05 13:26:29 -0400

    Time and Date
    notes added

--- Date_Time/Date_Time/Program.cs
+++ Date_Time/Date_Time/Program.cs
@@ -10,14 +10,14 @@ namespace Date_Time
     {
         static void Main(string[] args)
         {
-            DateTime current = DateTime.Now;
+            DateTime current = DateTime.Now;//declaring our DateTime 
             Console.WriteLine("At this moment in time, it is: " + current);
             Console.WriteLine("Enter a number: ");
-            string userNum = Console.ReadLine();
-            int userInt = Convert.ToInt32(userNum);
-            DateTime futureTime = current.AddHours(userInt);
+            string userNum = Console.ReadLine();//users input gets converted to a user string
+            int userInt = Convert.ToInt32(userNum);//userInt gets converted to int32
+            DateTime futureTime = current.AddHours(userInt);//this gives us current time
 
-            Console.WriteLine("In " + userInt + " hours, it will be " + futureTime);
+            Console.WriteLine("In " + userInt + " hours, it will be " + futureTime);//math equation to get users time and date adds the users input from the first question, returns future time specified by you. date and time + fututre time.
             Console.ReadLine();
         }
     }
