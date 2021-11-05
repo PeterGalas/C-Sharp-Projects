871fbc86956f186f153984391cf53f3f92cb7274
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-05 13:51:16 -0400

    Const Var
    page number 383

---
+++ Const_Var/Const_Var/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var
+{
+    public class Student
+    {
+        public Student() : this("", "")//public student class
+        {
+        }
+
+        public Student(string name, string city)
+        {
+            const string studentName = "Peter Galas";//student name
+            var State = "Floria";//const variable State lived in
+            var City = "North Port";//var const doesnt change = City
+            Console.WriteLine("Student name = {0}, State = {1}, City = {2}", studentName, State, City);//connecting everything relatiable together gives answers to our variables
+        }
+    }
+}
---
+++ Const_Var/Const_Var/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();//creating a student
+            Console.ReadLine();
+        }
+    }
+}
